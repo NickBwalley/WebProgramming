ed4f9a0f65db34809d5258b3175848825eb197cb
NickBwalley <nickbwalley@gmail.com>
2020-08-21 14:42:50 +0300

    Web

---
+++ PHP/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "";
+$db = "WebProgramming";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+
+if (!$conn) {
+
+   die("Connection failed: " . mysqli_connect_error());
+
+}
+
+echo "Connected successfully";
+
+?>
